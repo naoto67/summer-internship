35041ad0849e525a6faea502b252b54dabac4a05
naoto67 <uver.com67@gmail.com>
2019-07-25 18:54:46 +0900

    remove ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :steps, dependent: :destroy
-  has_many :ingredients, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
 
